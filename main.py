bd38bfe0718db7cc4f85d30721ea6b6aa7dd7eba
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 16:25:38 +0800

    Fix: added parentheses to calcculate level accurately and avoid error

--- main.py
+++ main.py
@@ -89,33 +89,33 @@ def check_position(centerx, centery):
             if level[(centery + num3) // num1][centerx// num2] < 3:
                 turns[3] = True
         if direction == 3:
-            if level[centery - num3 // num1][centerx // num2] < 3:
+            if level[(centery - num3) // num1][centerx // num2] < 3:
                 turns[2] = True
                 
         if direction == 2 or direction == 3:
             if 12 <= centerx % num2 <= 18:
-                if level[centery + num3//num1][centerx // num2] < 3:
+                if level[(centery + num3)//num1][centerx // num2] < 3:
                     turns[3] = True
-                if level[centery - num3//num1][centerx // num2] < 3:
+                if level[(centery - num3)//num1][centerx // num2] < 3:
                     turns[2] = True
             
             if 12 <= centery % num1 <= 18:
-                if level[centery//num1][centerx - num2 // num2] < 3:
+                if level[centery//num1][(centerx - num2) // num2] < 3:
                     turns[1] = True
-                if level[centery//num1][centerx + num2 // num2] < 3:
+                if level[centery//num1][(centerx + num2) // num2] < 3:
                     turns[0] = True
                     
         if direction == 0 or direction == 1:
             if 12 <= centerx % num2 <= 18:
-                if level[centery + num1//num1][centerx // num2] < 3:
+                if level[(centery + num3)//num1][centerx // num2] < 3:
                     turns[3] = True
-                if level[centery - num1//num1][centerx // num2] < 3:
+                if level[(centery - num3)//num1][centerx // num2] < 3:
                     turns[2] = True
             
             if 12 <= centery % num1 <= 18:
-                if level[centery//num1][centerx - num3 // num2] < 3:
+                if level[centery//num1][(centerx - num2) // num2] < 3:
                     turns[1] = True
-                if level[centery//num1][centerx + num3 // num2] < 3:
+                if level[centery//num1][(centerx + num2) // num2] < 3:
                     turns[0] = True
                     
     else:
