563901a3a7e17c202c5924cbf2f196094c7b11e5
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 11:35:11 +0800

    Add: drawboard func and started on dots and power up

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ pygame.init()
 
 # display screen and other necessities
 width = 900
-height = 950
+height = 700
 screen = pygame.display.set_mode([width, height])
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font("freesansbold.ttf", 20)
 level = boards
+
+def draw_board():
+    num1 = ((height - 50) // 32)
+    num2 = (width // 30)
+    
+    for row in range(len(level)): 
+        for col in range(len(level[row])):
+            if level[row][col] == 1:
+                pygame.draw.circle(screen, "white", (col * num2 + (0.5 * num2), row * num1 + (0.5 * num1)), 4)
+            if level[row][col] == 2:
+                pygame.draw.circle(screen, "white", (col * num2 + (0.5 * num2), row * num1 + (0.5 * num1)), 10)
+            
 
 # running display
 run = True
