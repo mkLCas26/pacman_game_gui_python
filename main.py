ac7f1030392c2c9aa91953349a87944ffd729481
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 11:02:14 +0800

    Add: running the display of the game and quit button

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ height = 950
 screen = pygame.display.set_mode([width, height])
 timer = pygame.time.Clock()
 fps = 60
-font = pygame.font.Font('freesansbold.ttf', 20)
+font = pygame.font.Font("freesansbold.ttf", 20)
+
+run = True
+while run: 
+    timer.tick(fps)
+    screen.fill("black")
+    
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            run = False
+            
+    pygame.display.flip()
+pygame.quit()
