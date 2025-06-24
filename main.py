9ce51c1e63f43282b4b247b77872fca393ac0f70
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 10:55:47 +0800

    Add: dimensions for the game display and other necessities

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import pygame
 
 pygame.init()
 
+width = 900
+height = 950
+screen = pygame.display.set_mode([width, height])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
