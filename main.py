451bc76f29935a4fefd1aa68f3cd7b07e28820dc
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 11:58:09 +0800

    Add: drew gate line and top right curve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import libraries and files
 import pygame
+import math
 from pacboard import boards
 
 # initialize pygame
@@ -14,7 +15,7 @@ fps = 60
 font = pygame.font.Font("freesansbold.ttf", 20)
 level = boards
 color = "blue"
-
+pi = math.pi
 
 def draw_board():
     num1 = ((height - 50) // 32)
@@ -32,7 +33,13 @@ def draw_board():
             if level[row][col] == 4:
                 pygame.draw.line(screen, color, (col * num2, row * num1 + (0.5 * num1)),
                                  ((col * num2) + num2, row * num1 + (0.5 * num1)), 3)
-            
+            if level[row][col] == 5:
+                pygame.draw.arc(screen, color, 
+                                [(col * num2 - (num2 * 0.4)) - 2, (row * num1 + (0.5 * num1)), num2, num1], 0, pi/2, 3)
+                
+            if level[row][col] == 9:
+                pygame.draw.line(screen, "white", (col * num2, row * num1 + (0.5 * num1)),
+                                 ((col * num2) + num2, row * num1 + (0.5 * num1)), 3)
 
 # running display
 run = True
